2d49da25a1ceaa464c5563080f7c993d67a94d9c
Verónica Rivadulla Deschamps <v.rivadulla.deschamps@gmail.com>
2024-11-27 12:02:38 +0100

    correc

--- delay_2.ino
+++ delay_2.ino
@@ -3,14 +3,13 @@ void setup() {
   Serial.begin(9600);
 }
 
-int ctr1 = 0;
-int ctr2 = 0;
-
 void loop() {
   // put your main code here, to run repeatedly:
+  int ctr1 = 0;
   ctr1 = ctr1+10;
   Serial.println("Contador 1: ");
   Serial.println(ctr1);
+  int ctr2 = 0;
   ctr2++;
   Serial.println("Contador 2: ");
   Serial.println(ctr2);
